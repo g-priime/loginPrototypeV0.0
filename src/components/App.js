dd36e87d3f39209077e895fe833eb7f1e43b7551
Hans Cabrera <44388744+Hawns4@users.noreply.github.com>
2020-02-02 16:30:19 -0700

    Merge branch 'master' of https://github.com/g-priime/loginPrototypeV0.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,39 @@
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
-import {BrowserRouter, Route } from 'react-router-dom';
 
-import unsplash from '../api/unsplash';
-//import ImageList from './ImageList';
-import PageOne from './PageOne';
-
-
-const PageTwo = () => {
-    return <div>PageTwo</div>;
-};
+import BasePath from '../api/BasePath';
+import RegisterPage from './RegisterPage';
+import HomePage from './HomePage';
 
 class App extends React.Component {
-    //state = { images: [] };
     state = { images: [] };
 
-    onSearchSubmit = async (term, password) => {
-        const response = await unsplash.put('/webresources/generic', { term , password });
-        //const response = await unsplash.get('/webresources/generic_1');
+    onSearchSubmit = async (username, password, fname, lname, email) => {
+        const response = await BasePath.put('/webresources/register', 
+        { username , password , fname , lname , email });
+
         console.log(response.data);
-        //const response = 'goodbye';
         this.setState({ images: response.data});
     }
 
     render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Route path="/SearchBar" exact component={PageOne} />
-                        <Route path="/pageTwo" component={PageTwo} />
-                    </div>
-                </BrowserRouter>
-            </div>
-            /*
-            <div className="ui container" style={{ marginTop: '10px'}}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
-                <div>{this.state.images}</div>
-                
-            </div>*/
-        );
+        var isValid = this.state.images;
+
+        if(isValid !== 'account registered'){
+            return (
+                <div className="ui container" style={{ marginTop: '10px'}}>
+                    <RegisterPage onSubmit={this.onSearchSubmit} />
+                    <div>{this.state.images}</div>                  
+                </div>
+            );    
+        } 
+        else {
+            return (
+                <div>
+                    <HomePage />
+                </div>
+            );
+        }
     }
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-//import Button from 'react-bootstrap';
-
 class HomePage extends React.Component {
 
     render() {
