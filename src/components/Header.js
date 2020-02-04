53491aa3566881d39969669eeafa527e6d9749fc
Holly Harnack <806547@ACDM.DS.SAIT.CA>
2020-02-04 10:56:55 -0700

    Create Header.js
    adding style

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from 'react-bootstrap';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <Navbar>
+                
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
