ddef14f6179661c40980fd354b4456e77bae60dd
Holly <hharnack@hotmail.com>
2020-02-06 08:44:19 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Navbar from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import HomePage from './HomePage';
+import RegisterPage from './RegisterPage';
 
 class Header extends React.Component {
 
     render() {
         return (
+            <div>
             <Navbar>
-                
+            <Navbar.Header>
+                <Navbar.Brand href={HomePage}>Dog</Navbar.Brand>
+            </Navbar.Header>
+                <Nav>
+                    <Nav.Link href={RegisterPage}>Register</Nav.Link>
+                    <Nav.Link href="">Services</Nav.Link>
+                    <Nav.Link href="">Gallery</Nav.Link>
+                </Nav>
             </Navbar>
+            </div>
         );
     }
 }
