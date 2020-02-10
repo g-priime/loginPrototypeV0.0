4d27856a456ae3f46d310f7d2fd84237fcd45418
Holly Harnack <806547@ACDM.DS.SAIT.CA>
2020-02-10 10:23:55 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ class Header extends React.Component {
         return (
             <div>
             <div className="d-flex justify-content-between">
-                <img src={dog} height="30" width="50" className="align-self-center" onClick={() => {this.props.onChangePage('home')}}Dog Logo/>
+                <img src={dog} height="30" width="40" className="align-self-center" onClick={() => {this.props.onChangePage('home')}}Dog Logo/>
                 <h1 className="title pt-0 mt-0">K9 FUN FAMILY</h1>
             </div>
             <div className="pt-3 pb-3 d-flex justify-content-around header" style={{ backgroundColor: "#ECEBE7" }}>
