feb813e3feed11ecf729ed7cb993e5e9545fcd9b
Holly Harnack <806547@ACDM.DS.SAIT.CA>
2020-02-10 10:55:03 -0700

    Create Gallery.js

---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Gallery extends React.Component {
+
+    render() {
+        return (
+            <div className="ui segment">
+                Gallery
+            </div>
+        );
+    }
+}
+
+export default Gallery;
