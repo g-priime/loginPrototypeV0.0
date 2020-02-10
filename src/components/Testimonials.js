17d7e904ac5166b955310818a65064b2024bad1c
Holly Harnack <806547@ACDM.DS.SAIT.CA>
2020-02-10 10:55:08 -0700

    Create Testimonials.js

---
+++ src/components/Testimonials.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Testimonials extends React.Component {
+
+    render() {
+        return (
+            <div className="ui segment">
+                Testimonials
+            </div>
+        );
+    }
+}
+
+export default Testimonials;
