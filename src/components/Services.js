b6b6c6eb8461bde8bf54cd7f1f0a3599f22f4b50
Holly Harnack <806547@ACDM.DS.SAIT.CA>
2020-02-10 10:55:06 -0700

    Create Services.js

---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Services extends React.Component {
+
+    render() {
+        return (
+            <div className="ui segment">
+                Services
+            </div>
+        );
+    }
+}
+
+export default Services;
