92fd34f4359922fdc76f14a2f25cb2778d83730e
Holly Harnack <806547@ACDM.DS.SAIT.CA>
2020-02-11 08:35:23 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -5,6 +5,10 @@ import RegisterPage from './RegisterPage';
 import HomePage from './HomePage';
 import Header from './Header';
 import RegisterMain from './RegisterMain';
+import Gallery from './Gallery';
+import FAQ from './FAQ';
+import Testimonials from './Testimonials';
+import Services from './Services';
 import './main.css';
 
 import { BrowserRouter, Route, Link } from 'react-router-dom';
@@ -21,6 +25,10 @@ class App1 extends React.Component {
                     <Route path="/" exact 
                     component={HomePage} />
                     <Route path="/Register" component={RegisterMain} />
+                    <Route path="/Services" component={Services} />
+                    <Route path="/Gallery" component={Gallery} />
+                    <Route path="/Testimonials" component={Testimonials} />
+                    <Route path="/FAQ" component={FAQ} />
                 </div>
                 </BrowserRouter>
             </div>
