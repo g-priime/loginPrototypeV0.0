39c5e8b4d3b204241e6cc0c559fe449fae03fc1b
Holly <hharnack@hotmail.com>
2020-02-13 10:02:38 -0700

    Update Header1.js

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -19,17 +19,17 @@ class Header1 extends React.Component {
             <div>
             <div className="d-flex justify-content-between">
                 <Link to="/">
-                <img src={dog} height="30" width="40" className="align-self-end"/>
+                <img src={dog} height="30" width="50" className="align-self-end"/>
                 </Link>
                 <h2 className="title mr-3">K9 FUN FAMILY</h2>
             </div>
             <div className="container-head d-flex align-items-center d-flex justify-content-around header" style={{ backgroundColor: "#ECEBE7" }}>
-                            <div><Link to="/" style={{ color: "#707070" }} ><div className="pt-2 pb-2">Home</div></Link></div>
-                            <div><Link to="/Services" style={{ color: "#707070" }}><div className="pt-2 pb-2">Services</div></Link></div>
-                            <div><Link to="/Gallery" style={{ color: "#707070" }}><div className="pt-2 pb-2">Gallery</div></Link></div>
-                            <div><Link to="/Testimonials" style={{ color: "#707070" }}><div className="pt-2 pb-2">Testimonials</div></Link></div>
-                            <div><Link to="/FAQ" style={{ color: "#707070" }}><div className="pt-2 pb-2">FAQ</div></Link></div>
-                            <div><Link to="/Register" style={{ color: "#707070" }}><div className="pt-2 pb-2">Login/Register</div></Link></div>
+                            <div><Link to="/" style={{ color: "#707070" }} ><div className="pt-3 pb-3">Home</div></Link></div>
+                            <div><Link to="/Services" style={{ color: "#707070" }}><div className="pt-3 pb-3">Services</div></Link></div>
+                            <div><Link to="/Gallery" style={{ color: "#707070" }}><div className="pt-3 pb-3">Gallery</div></Link></div>
+                            <div><Link to="/Testimonials" style={{ color: "#707070" }}><div className="pt-3 pb-3">Testimonials</div></Link></div>
+                            <div><Link to="/FAQ" style={{ color: "#707070" }}><div className="pt-3 pb-3">FAQ</div></Link></div>
+                            <div><Link to="/Register" style={{ color: "#707070" }}><div className="pt-3 pb-3">Login/Register</div></Link></div>
             
             </div>
             </div>
