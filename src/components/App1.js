ba2261a381b3d9ea97739e7d4a33189ed8bc3699
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-02-18 21:19:17 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import BasePath from '../api/BasePath';
-import RegisterPage from './RegisterPage';
 import HomePage from './HomePage';
-import Header from './Header';
 import RegisterMain from './RegisterMain';
 import Gallery from './Gallery';
 import FAQ from './FAQ';
@@ -11,7 +8,7 @@ import Testimonials from './Testimonials';
 import Services from './Services';
 import './main.css';
 
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header1 from './Header1';
 
 class App1 extends React.Component {
