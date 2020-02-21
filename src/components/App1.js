824f1d6927749bcc66bda82dde81096eff02e729
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-02-21 16:15:47 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 import HomePage from './HomePage';
 import RegisterMain from './register/RegisterMain';
-//import Gallery from './Gallery';
+import Gallery from './Gallery';
 import FAQ from './FAQ';
 import Testimonials from './Testimonials';
-//import Services from './Services';
+import Services from './Services';
 import '../css/main.css';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header1 from './Header1';
 
-import EditCustomerMain from './editCustomer/EditCustomerMain';
-import ChangePasswordMain from './changePassword/ChangePasswordMain';
+//import EditCustomerMain from './editCustomer/EditCustomerMain';
+//import ChangePasswordMain from './changePassword/ChangePasswordMain';
 
 class App1 extends React.Component {
 
@@ -26,8 +26,8 @@ class App1 extends React.Component {
                     <Route path="/" exact 
                     component={HomePage} />
                     <Route path="/Register" component={RegisterMain} />
-                    <Route path="/Services" component={EditCustomerMain} />
-                    <Route path="/Gallery" component={ChangePasswordMain} />
+                    <Route path="/Services" component={Services} />
+                    <Route path="/Gallery" component={Gallery} />
                     <Route path="/Testimonials" component={Testimonials} />
                     <Route path="/FAQ" component={FAQ} />
                     </div>
