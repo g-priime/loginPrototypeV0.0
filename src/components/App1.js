dfc731c2256293bde42a04a1607e90a54aba0be1
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-02-21 12:03:06 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import HomePage from './HomePage';
 import RegisterMain from './register/RegisterMain';
-import Gallery from './Gallery';
+//import Gallery from './Gallery';
 import FAQ from './FAQ';
 import Testimonials from './Testimonials';
 //import Services from './Services';
@@ -12,6 +12,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header1 from './Header1';
 
 import EditCustomerMain from './editCustomer/EditCustomerMain';
+import ChangePasswordMain from './changePassword/ChangePasswordMain';
 
 class App1 extends React.Component {
 
@@ -26,7 +27,7 @@ class App1 extends React.Component {
                     component={HomePage} />
                     <Route path="/Register" component={RegisterMain} />
                     <Route path="/Services" component={EditCustomerMain} />
-                    <Route path="/Gallery" component={Gallery} />
+                    <Route path="/Gallery" component={ChangePasswordMain} />
                     <Route path="/Testimonials" component={Testimonials} />
                     <Route path="/FAQ" component={FAQ} />
                     </div>
