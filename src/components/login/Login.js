dfc08ce1589a293369bca475b4a08bc1f03e4ed8
Holly <hharnack@hotmail.com>
2020-02-25 16:25:23 -0700

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -8,6 +8,10 @@ import Modal from 'react-bootstrap/Modal';
 
 class Login extends React.Component {
 
+  constructor(props) {
+    super(props);
+  };
+
   state = {
     username: "",
     password: ""
@@ -18,35 +22,38 @@ class Login extends React.Component {
       username: this.state.username,
       password: this.state.password
     })
-    .then( result => {
-      console.log(result);
-    });
+      .then(result => {
+        if (result.data != "invalid login") {
+          localStorage.setItem('token', result.data);
+          console.log(localStorage.getItem('token'));
+        } else {
+          console.log('invalid');
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      });
   };
 
   render() {
     return (
-      //   <Modal
-      //   size="lg"
-      //   aria-labelledby="contained-modal-title-vcenter"
-      //   centered
-      // >
-      //   <Modal.Header closeButton>
-      //     <Modal.Title id="contained-modal-title-vcenter">
-      //       Login/Register
-      //     </Modal.Title>
-      //   </Modal.Header>
-      //   <Modal.Body>
-      <div >
-      <h1 > Login </h1>
-      Enter Username: < input type = "text" onChange = {(event, newValue) => this.setState({username: newValue})}/>
-      Enter Password: < input type = "password" onChange = {(event, newValue) => this.setState({password: newValue})}/> 
-      <button onClick = {this.loginUser}>Login</button> 
-      </div>
-      //  </Modal.Body>
-      //   <Modal.Footer>
-      //     <button onClick={this.loginUser}>Login</button>
-      //   </Modal.Footer>
-      // </Modal>
+
+      <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Login/Register
+           </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div >
+            <h1 > Login </h1>
+            Enter Username: < input type="text" onChange={(event) => this.setState({ username: event.target.value })} />
+            Enter Password: < input type="password" onChange={(event) => this.setState({ password: event.target.value })} />
+            <button onClick={this.loginUser}>Login</button>
+          </div>
+        </Modal.Body>
+      </Modal>
+
     );
   }
 }
