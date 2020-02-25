cc30827b0c1ec217df72d78456949abbd27c261a
Holly <hharnack@hotmail.com>
2020-02-25 10:08:57 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -6,6 +6,7 @@ import Gallery from './Gallery';
 import FAQ from './FAQ';
 import Testimonials from './Testimonials';
 import Services from './Services';
+import Login from './login/Login'
 import '../css/main.css';
 
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -23,9 +24,8 @@ class App1 extends React.Component {
                 <div>
                     <Header1 />
                     <div className="mt-4 ml-5 mr-5">
-                    <Route path="/" exact 
-                    component={HomePage} />
-                    <Route path="/Register" component={RegisterMain} />
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/Login" component={Login} />
                     <Route path="/Services" component={Services} />
                     <Route path="/Gallery" component={Gallery} />
                     <Route path="/Testimonials" component={Testimonials} />
