ee47837076faa44f986a82b2594a9b0c4111007c
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-02-25 09:08:33 -0700

    Create UserAccount.js

---
+++ src/components/UserAccount.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "../css/reg.css";
+import "../css/userAccount.css";
+
+class UserAccount extends React.Component {
+  state = {
+    dogname: "",
+    breed: "",
+    dob: "",
+    gender: "",
+    weight: "",
+    neuteredspayed: "",
+    medication: "",
+    allergies: "",
+    physlimit: "",
+    veterinarian: "",
+    straingers: "",
+    largerdogs: "",
+    smalldogs: "",
+    puppies: "",
+    da2pp: "",
+    rabies: "",
+    bordetella: ""
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(
+      this.state.dogname,
+      this.state.breed,
+      this.state.dob,
+      this.state.gender,
+      this.state.weight,
+      this.state.neuteredspayed,
+      this.state.medication,
+      this.state.allergies,
+      this.state.physlimit,
+      this.state.veterinarian,
+      this.state.straingers,
+      this.state.largerdogs,
+      this.state.smalldogs,
+      this.state.puppies,
+      this.state.da2pp,
+      this.state.rabies,
+      this.state.bordetella
+    );
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm">
+          <div
+            className="ui segment p-3 mb-2 "
+            style={{ backgroundColor: "#ECEBE7" }}
+          >
+            <button
+              className="btn "
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+              }}
+            >
+              Edit customer information
+            </button>
+            <br /><br />
+            <button
+              className="btn "
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+              }}
+            >
+              Change password
+            </button>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div
+            className="ui segment p-3 mb-2 "
+            style={{ backgroundColor: "#ECEBE7" }}
+          ></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserAccount;
