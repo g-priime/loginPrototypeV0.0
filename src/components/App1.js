71eb3560009ccea520825236c1e9f9633bcb4383
g-priime <57049481+g-priime@users.noreply.github.com>
2020-02-29 11:33:19 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -12,7 +12,7 @@ import '../css/main.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header1 from './Header1';
 
-//import EditCustomerMain from './editCustomer/EditCustomerMain';
+import EditCustomerMain from './editCustomer/EditCustomerMain';
 import ChangePasswordMain from './changePassword/ChangePasswordMain';
 
 class App1 extends React.Component {
@@ -44,7 +44,7 @@ class App1 extends React.Component {
                         <div className="mt-4 ml-5 mr-5">
                             <Route path="/" exact component={HomePage} />
                             <Route path="/Register" component={RegisterMain} />
-                            <Route path="/Services" component={Services} />
+                            <Route path="/Services" component={EditCustomerMain} />
                             <Route path="/Gallery" component={Gallery} />
                             <Route path="/Testimonials" component={Testimonials} />
                             <Route path="/FAQ" component={FAQ} />
