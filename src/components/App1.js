68bd145b365c62512d2c97eec7bfc36f0c5d0196
Holly <hharnack@hotmail.com>
2020-03-03 09:35:49 -0700

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -6,15 +6,16 @@ import Gallery from './Gallery';
 import FAQ from './FAQ';
 import Testimonials from './Testimonials';
 import Services from './Services';
-import Login from './login/Login'
+import Login from './login/Login';
+import BasePath from './../api/BasePath';
 import '../css/main.css';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header1 from './Header1';
 import EditCustomerMain from './editCustomer/EditCustomerMain';
 import EditCustomer1 from './editCustomer/EditCustomerMain';
-//import EditCustomerMain from './editCustomer/EditCustomerMain';
 import ChangePasswordMain from './changePassword/ChangePasswordMain';
+import { getQueriesForElement } from '@testing-library/react';
 
 class App1 extends React.Component {
 
@@ -37,6 +38,7 @@ class App1 extends React.Component {
     }
 
     render() {
+ 
         return (
             <div>
                 <BrowserRouter>
@@ -46,11 +48,12 @@ class App1 extends React.Component {
                         <div className="mt-4 ml-5 mr-5">
                             <Route path="/" exact component={HomePage} />
                             <Route path="/Register" component={RegisterMain} />
-                            <Route path="/Services" component={Services} />
+                            <Route path="/Services" component={UserAccount}/>
                             <Route path="/Gallery" component={Gallery} />
                             <Route path="/Testimonials" component={Testimonials} />
                             <Route path="/FAQ" component={FAQ} />
                             <Route path="/ChangePass" component={ChangePasswordMain}/>
+                            <Route path="/EditCustomer" component={EditCustomerMain}/>
                             <Login
                                 show={this.state.show}
                                 onHide={this.onHide}
