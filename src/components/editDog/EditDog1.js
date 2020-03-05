22ae728493d7aaf66abbf41963fa92ae2968f581
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-05 13:09:10 -0700

    Update EditDog1.js

--- src/components/editDog/EditDog1.js
+++ src/components/editDog/EditDog1.js
@@ -235,7 +235,7 @@ class EditDog1 extends React.Component {
           </div>
           <div className="d-flex justify-content-between">
             <Link //creates a link, styled like a button
-              to="/" //telling to go to home, in adddog it should be accinfo
+              to="/UserAccount" //telling to go to home, in adddog it should be accinfo
               type="button"
               className="btn mb-3"
               style={{
