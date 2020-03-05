f356aaa4d4d46bf14a63688ee1b2c56592d8e44e
Holly <hharnack@hotmail.com>
2020-03-05 12:19:31 -0700

    Update Header1.js

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -5,185 +5,182 @@ import { NavLink } from "react-router-dom";
 
 import BasePath from "../api/BasePath";
 import {
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from "reactstrap";
 
 class Header1 extends React.Component {
-  state = {
-    initialStates: false,
-    username: ""
-  };
+    state = {
+        initialStates: false,
+        username: ""
+    };
 
-  getCustomerInfo = async () => {
-    var token = localStorage.getItem("token");
+    getCustomerInfo = async () => {
+        var token = localStorage.getItem("token");
 
-    const customerInfo = await BasePath.get(
-      `/webresources/RetrieveUser/${token}`
-    );
+        const customerInfo = await BasePath.get(
+            `/webresources/RetrieveUser/${token}`
+        );
 
-    if (
-      this.state.initialStates === false &&
-      customerInfo.data !== "Authentication error, bad token"
-    ) {
-      this.setState({
-        initialStates: true,
-        username: customerInfo.data.username
-      });
-    }
-  };
+        if (
+            this.state.initialStates === false &&
+            customerInfo.data !== "Authentication error, bad token"
+        ) {
+            this.setState({
+                initialStates: true,
+                username: customerInfo.data.username
+            });
+        }
+    };
 
-  logOut = () => {
-    localStorage.clear();
-    this.setState({ initialStates: false, username: "" });
-  };
+    logOut = () => {
+        localStorage.clear();
+        this.setState({ initialStates: false, username: "" });
+    };
 
-  render() {
-    this.getCustomerInfo();
-    var username = this.state.username;
+    render() {
+        this.getCustomerInfo();
+        var username = this.state.username;
 
-    if (username !== "") {
-      return (
-        <div>
-          <div className="d-flex justify-content-between">
-            <NavLink to="/">
-              <img
-                src={dog}
-                alt="dog"
-                height="30"
-                width="40"
-                className="align-self-end"
-              />
-            </NavLink>
-            <h2 className="title mr-3">K9 FUN FAMILY</h2>
-          </div>
-          {/* TODO: find a way to keep track of current page for highlighting the tab, maybe store in session? */}
-          <div
-            className="container-head d-flex justify-content-around header"
-            style={{ backgroundColor: "#ECEBE7" }}
-          >
-                        
-                    <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} to="/" style={{ color: "#707070" }} >Home</NavLink>
-            <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} to="/Services" style={{ color: "#707070" }} >Services</NavLink>
-                    <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} to="/Gallery" style={{ color: "#707070" }} >Gallery</NavLink>
-                    <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} to="/Testimonials" style={{ color: "#707070" }} >Testimonials</NavLink>
-                    <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} to="/FAQ" style={{ color: "#707070" }} >FAQ</NavLink>
-            <div className="dropDown">
-              <UncontrolledDropdown>
-                <DropdownToggle nav caret style={{ color: "#707070" }}>
-                  <div className="pt-3 pb-3">Account</div>
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    <NavLink to="/Profile" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}>
-                      Profile
-                    </NavLink>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <NavLink
-                      to="/ViewAppointments"
-                      exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}
+        if (username !== "") {
+            return (
+                <div>
+                    <div className="d-flex justify-content-between">
+                        <NavLink to="/">
+                            <img
+                                src={dog}
+                                alt="dog"
+                                height="30"
+                                width="40"
+                                className="align-self-end"
+                            />
+                        </NavLink>
+                        <h2 className="title mr-3">K9 FUN FAMILY</h2>
+                    </div>
+                    {/* TODO: find a way to keep track of current page for highlighting the tab, maybe store in session? */}
+                    <div
+                        className="container-head d-flex justify-content-around header"
+                        style={{ backgroundColor: "#ECEBE7" }}
                     >
-                      View Appointments
+
+                        <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} to="/" style={{ color: "#707070" }} >Home</NavLink>
+                        <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} to="/Services" style={{ color: "#707070" }} >Services</NavLink>
+                        <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} to="/Gallery" style={{ color: "#707070" }} >Gallery</NavLink>
+                        <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} to="/Testimonials" style={{ color: "#707070" }} >Testimonials</NavLink>
+                        <NavLink className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} to="/FAQ" style={{ color: "#707070" }} >FAQ</NavLink>
+                        <div className="dropDown">
+                            <UncontrolledDropdown>
+                                <DropdownToggle nav caret style={{ color: "#707070" }}>
+                                    <div className="pt-3 pb-3">Account</div>
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        <NavLink to="/Profile" exact activeClassName="active" activeStyle={{
+                                            color: "#FEF6F6"
+                                        }} style={{ color: "#707070" }}>
+                                            Profile
                     </NavLink>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    <NavLink
-                      onClick={this.logOut}
-                      to="/"
-                      exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <NavLink
+                                            to="/ViewAppointments"
+                                            exact activeClassName="active" activeStyle={{
+                                                color: "#FEF6F6"
+                                            }} style={{ color: "#707070" }}
+                                        >
+                                            View Appointments
+                    </NavLink>
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        <NavLink
+                                            onClick={this.logOut}
+                                            to="/"
+                                            exact activeClassName="active" activeStyle={{
+                                                color: "#FEF6F6"
+                                            }} style={{ color: "#707070" }}
+                                        >
+                                            Log Out
+                    </NavLink>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <div className="d-flex justify-content-between">
+                        <NavLink to="/">
+                            <img
+                                src={dog}
+                                alt="dog"
+                                height="30"
+                                width="40"
+                                className="align-self-end"
+                            />
+                        </NavLink>
+                        <h2 className="title mr-3">K9 FUN FAMILY</h2>
+                    </div>
+                    {/* TODO: find a way to keep track of current page for highlighting the tab, maybe store in session? */}
+                    <div
+                        className="container-head d-flex justify-content-around header"
+                        style={{ backgroundColor: "#ECEBE7" }}
                     >
-                      Log Out
-                    </NavLink>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="d-flex justify-content-between">
-            <NavLink to="/">
-              <img
-                src={dog}
-                alt="dog"
-                height="30"
-                width="40"
-                className="align-self-end"
-              />
-            </NavLink>
-            <h2 className="title mr-3">K9 FUN FAMILY</h2>
-          </div>
-          {/* TODO: find a way to keep track of current page for highlighting the tab, maybe store in session? */}
-          <div
-            className="container-head d-flex justify-content-around header"
-            style={{ backgroundColor: "#ECEBE7" }}
-          >
-            <div>
-              <NavLink to="/" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}>
-                Home
+
+                        <NavLink to="/" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} style={{ color: "#707070" }}>
+                            Home
               </NavLink>
-            </div>
-            <div>
-              <NavLink to="/Services" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}>
-                Services
+
+
+                        <NavLink to="/Services" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} style={{ color: "#707070" }}>
+                            Services
               </NavLink>
-            </div>
-            <div>
-              <NavLink to="/Gallery" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}>
-                Gallery
+
+                        <NavLink to="/Gallery" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} style={{ color: "#707070" }}>
+                            Gallery
               </NavLink>
-            </div>
-            <div>
-              <NavLink to="/Testimonials" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}>
-                Testimonials
+
+                        <NavLink to="/Testimonials" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} style={{ color: "#707070" }}>
+                            Testimonials
               </NavLink>
-            </div>
-            <div>
-              <NavLink to="/FAQ" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
-                        color: "#FEF6F6"
-                    }} style={{ color: "#707070" }}>
-                FAQ
+
+                        <NavLink to="/FAQ" className="pt-3 flex-grow-1 bg" exact activeClassName="active" activeStyle={{
+                            color: "#FEF6F6"
+                        }} style={{ color: "#707070" }}>
+                            FAQ
               </NavLink>
-            </div>
-            <div style={{ color: "#707070" }} onClick={this.props.showLogin}>
-              <div className="pt-3 pb-3">Login/Register</div>
-            </div>
-          </div>
-        </div>
-      );
+
+                        <div style={{ color: "#707070" }} onClick={this.props.showLogin}>
+                            Login/Register
+                        </div>
+                    </div>
+                </div>
+            );
+        }
     }
-  }
 }
 
 export default Header1;
