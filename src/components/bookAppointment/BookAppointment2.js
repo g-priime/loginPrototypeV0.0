73949cc5f243a19d39a1e1176f68ff88f2dc1763
g-priime <57049481+g-priime@users.noreply.github.com>
2020-03-07 10:47:44 -0700

    Create BookAppointment2.js

---
+++ src/components/bookAppointment/BookAppointment2.js
@@ -0,0 +1,111 @@
+import React from "react";
+import "../../css/changePassword.css";
+import { Link } from "react-router-dom";
+
+class BookAppointment2 extends React.Component {
+  state = {
+    dog: "",
+    startTime: "",
+    endTime: "",
+
+    dogs: [],
+    comments: ""
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.dog);
+
+    this.props.onSubmit(
+      this.state.dog,
+      this.state.startTime,
+      this.state.endTime,
+      this.state.grooming,
+      this.state.comments
+    );
+  };
+
+  render() {
+    return (
+      <div
+        className="ui segment contChangestartTime"
+        style={{ backgroundColor: "#ECEBE7" }}
+      >
+        <form
+          onSubmit={this.onFormSubmit}
+          className="ui form"
+          style={{ backgroundColor: "#ECEBE7 " }}
+        >
+          <div className="container">
+            <h1>Appointment Details</h1>
+
+            <div className="row">
+              <div className="col-sm">
+                <label>Dog:</label>
+                {this.props.dog}
+                <br />
+                <br />
+
+                <label>Start Time:</label>
+                {this.props.startTime}
+                <br />
+                <br />
+
+                <label>End Time:</label>
+                {this.props.endTime}
+                <br />
+                <br />
+
+                <label>Grooming Added:</label>
+                {this.props.grooming}
+                <br />
+                <br />
+
+                <label>Additional Comments:</label>
+                {this.props.comments}
+
+                <label>Cost:</label>
+                $1,000,000,000.00
+              </div>
+              <br />
+              <br />
+            </div>
+            <br />
+
+            <div className="d-flex justify-content-between">
+              <Link
+                to="/"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+                }}
+              >
+                Cancel
+              </Link>
+              <button
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+                }}
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default BookAppointment2;
