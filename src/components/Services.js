659e41b44c230c888b3c9b224cfc7b368635840a
Holly <hharnack@hotmail.com>
2020-03-07 15:54:22 -0700

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 class Services extends React.Component {
 
     render() {
         return (
-            <div className="ui segment">
-                Services
+            <div className="ui segment d-flex justify-content-around">
+                <Card className="m-3">
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Book Now</Button>
+                    </CardBody>
+                </Card>
+                <Card className="m-3">
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Book Now</Button>
+                    </CardBody>
+                </Card>
+                <Card className="m-3">
+                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Book Now</Button>
+                    </CardBody>
+                </Card>
             </div>
         );
     }
