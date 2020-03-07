78c99f4dd8271e9385b8ff798006119ce726e2a5
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-07 14:26:14 -0700

    Update ViewAppointments.js

--- src/components/ViewAppointments.js
+++ src/components/ViewAppointments.js
@@ -57,6 +57,44 @@ class ViewAppointments extends React.Component {
           </div>
         </div>
         <br />
+        <div className="row ">
+          <div className="col-sm right">
+            <b>AppointmentType</b>
+          </div>
+          <div className="col-sm left">StartTime</div>
+          <div className="col-sm left">EndTime</div>
+          <div className="col-sm left">
+            <button
+              className="btn mb-3"
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                width: 150
+              }}
+            >
+              Edit
+            </button>
+          </div>
+          <div className="col-sm left">
+            <button
+              className="btn mb-3"
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                width: 150
+              }}
+            >
+              Make Payment
+            </button>
+          </div>
+        </div>
+        <br />
         <br />
         <div className="row">
           <div className="col-sm-1 right">
