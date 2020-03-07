a63cc16b7ab95974c0ce51f6998075083bdce414
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-07 13:51:15 -0700

    Update ViewAppointments.js

--- src/components/ViewAppointments.js
+++ src/components/ViewAppointments.js
@@ -3,6 +3,36 @@ import "../css/reg.css";
 import "../css/viewAppointments.css";
 import { Link } from "react-router-dom";
 
+import { ReactComponent as Hint } from "./hint.svg";
+const hint = () => (
+  <div>
+    {
+      <svg width="25" height="25">
+        <circle
+          cx="11"
+          cy="11"
+          r="10"
+          stroke="gray"
+          stroke-width="1"
+          fill="gray"
+        />
+        <text
+          x="6"
+          y="19"
+          font-size="22px "
+          fill="white"
+          font-weight="bold"
+          font-family="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
+        >
+          ?
+        </text>
+        Sorry, your browser does not support inline SVG.
+      </svg>
+    }
+    <Hint />
+  </div>
+);
+
 class ViewAppointments extends React.Component {
   render() {
     return (
@@ -15,7 +45,43 @@ class ViewAppointments extends React.Component {
         </div>
         <br />
         <br />
-     <div  className="row leftPad">Approved <hr width="70%" size="8" align="center"></hr></div>   
+
+        <div className="row ">
+          <div className="col-sm-1 right">
+            <Hint />{" "}
+          </div>
+          <div className="col-sm-2 left ">Approved</div>
+          <div className="col-sm left">
+            {" "}
+            <hr width="60%" size="8" align="center"></hr>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-sm-1 right">
+            <Hint />{" "}
+          </div>
+          <div className="col-sm-2 left">Pending</div>
+          <div className="col-sm left">
+            {" "}
+            <hr width="60%" size="8" align="center"></hr>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-sm-1 right">
+            <Hint />{" "}
+          </div>
+          <div className="col-sm-2 left">Billing Information</div>
+          <div className="col-sm left">
+            {" "}
+            <hr width="60%" size="8" align="center"></hr>
+          </div>
+        </div>
+        <br />
+        <br />
       </div>
     );
   }
