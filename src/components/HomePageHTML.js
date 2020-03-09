9ce2034e335baf7b0ce77915f60f427f6850291f
Viktoriia Davydova <viktoriia.davydova@edu.sait.ca>
2020-03-09 14:13:35 -0600

    Update HomePageHTML.js

--- src/components/HomePageHTML.js
+++ src/components/HomePageHTML.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "../css/reg.css";
 import "../css/homePage.css";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 class HomePageHTML extends React.Component {
   render() {
@@ -10,7 +19,20 @@ class HomePageHTML extends React.Component {
           <div
             className="ui segment p-3 mb-2 "
             style={{ backgroundColor: "#ECEBE7" }}
-          ></div>
+          >
+            <Card className="cardbg" >
+              <CardImg
+                className="images"
+                top
+                width="100%"
+                src={require("../images/welcome.jpg")}
+                alt="Card image cap"
+              />
+              <CardBody>
+                
+              </CardBody>
+            </Card>
+          </div>
         </div>
         <div className="col-sm-8">
           <div
