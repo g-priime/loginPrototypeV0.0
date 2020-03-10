1046fb7992381f168e72ad334d8d6a96c65cb15e
Holly <hharnack@hotmail.com>
2020-03-10 08:24:24 -0600

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -14,7 +14,7 @@ class Services extends React.Component {
                 <Card className="cardbg">
                     <CardImg className="img-fluid images" top width="100%" src={require("../images/boarding.jpg")} alt="Card image cap" />
                     <CardBody>
-                        <CardTitle><h1>Boarding</h1></CardTitle>
+                        <CardTitle><h1 style={{ color: "#715d5c" }}>Boarding</h1></CardTitle>
                         <CardText className="left">
                         We board friendly and vaccinated dogs in our home with our own dogs and treat them like a 
                         member of the family! That means plenty of potty breaks, play time in our large fenced 
@@ -22,7 +22,20 @@ class Services extends React.Component {
                         injections and blind or deaf dogs are no worry for us; we are happy to accommodate 
                         them for no additional charge.
                         </CardText>
-                        <CardSubtitle><h4>What to bring</h4></CardSubtitle>
+                        <CardSubtitle><h4 style={{ color: "#715d5c" }}>Requirements</h4></CardSubtitle><br></br>
+                        <CardText className="left">
+                        <ul>
+                        <li>Dogs must be dog friendly</li>
+
+                        <li>Adults must be house trained and able to be crated, or non-destructive</li>
+
+                        <li>All dogs must be up to date on distemper, bordetella and rabies vaccines for their age</li>
+
+                        <li>We will happily send picture or video updates at your request.</li>
+
+                        </ul>
+                        </CardText>
+                        <CardSubtitle><h4 style={{ color: "#715d5c" }}>What to bring</h4></CardSubtitle><br></br>
                         <CardText className="left">
                         <ul>
                         <li>Enough food for the duration of your dog’s stay</li>
@@ -35,14 +48,16 @@ class Services extends React.Component {
 
                         <li>No need to bring crates, bowls, or dog beds- we have plenty of those here!</li>
                         </ul>
-                        </CardText>
+                        </CardText><br></br>
+                        <CardSubtitle><h4 style={{ color: "#715d5c" }}>$50 +5% GST per dog per night</h4></CardSubtitle>
+                        <CardText><small className="text-muted">We offer discounts for longer stays</small></CardText><br></br>
                         <Button className="button">Book Now</Button>
                     </CardBody>
                 </Card>
                 <Card className="cardbg">
                     <CardImg className="img-fluid images" top width="100%" src={require("../images/training.jpg")} alt="Card image cap" />
                     <CardBody>
-                        <CardTitle><h1>Training</h1></CardTitle>
+                        <CardTitle><h1 style={{ color: "#715d5c" }}>Training</h1></CardTitle>
                         <CardText>In the Calgary area and need professional training for your dog(s)? We provide a great training program for dogs inclduing 
                             basic tasks. Just register and book a training appointment.</CardText>
                         <Button className="button">Book Now</Button>
@@ -51,7 +66,7 @@ class Services extends React.Component {
                 <Card className="cardbg">
                     <CardImg className="img-fluid images" top width="100%" src={require("../images/day.jpg")} alt="Card image cap" />
                     <CardBody>
-                        <CardTitle><h1>Day Care</h1></CardTitle>
+                        <CardTitle><h1 style={{ color: "#715d5c" }}>Day Care</h1></CardTitle>
                         <CardText>In the Calgary area and need somewhere to send your dog somewhere for one day? We can 
                             take care of your dog(s) for the day while you go to work or a school.
                             We will make sure they get the best care during their stay. Just register and book a day care appointment.</CardText>
