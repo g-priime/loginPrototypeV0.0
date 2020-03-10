a507df36fbc5444f535575a74197f8c7686fd6b2
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-10 14:13:44 -0600

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -5,7 +5,7 @@ import RegisterMain from './register/RegisterMain';
 import Gallery from './Gallery';
 import FAQ from './FAQ';
 import Testimonials from './Testimonials';
-//import Services from './Services';
+import Services from './Services';
 import Login from './login/Login'
 import '../css/main.css';
 
@@ -18,7 +18,7 @@ import EditCustomerMain from './editCustomer/EditCustomerMain';
 //import AddDog2 from './addDog/AddDog2';
 
 import ChangePasswordMain from './changePassword/ChangePasswordMain';
-import BookAppointmentMain from './bookAppointment/BookAppointmentMain';
+//import BookAppointmentMain from './bookAppointment/BookAppointmentMain';
 
 class App1 extends React.Component {
 
@@ -50,7 +50,7 @@ class App1 extends React.Component {
                         <div className="mt-4 ml-5 mr-5">
                             <Route path="/" exact component={HomePage} />
                             <Route path="/Register" component={RegisterMain} />
-                            <Route path="/Services" component={BookAppointmentMain} />
+                            <Route path="/Services" component={Services} />
                             <Route path="/Gallery" component={Gallery} />
                             <Route path="/Testimonials" component={Testimonials} />
                             <Route path="/FAQ" component={FAQ} />
