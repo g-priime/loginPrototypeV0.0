507c8d43694a9be55a748b1fada0e2f77a42b85f
g-priime <57049481+g-priime@users.noreply.github.com>
2020-03-13 08:44:39 -0600

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -1,10 +1,10 @@
 import React from "react";
 //import UserAccount from './UserAccount';
-import HomePage from "./HomePage";
+//import HomePage from "./HomePage";
 import RegisterMain from "./register/RegisterMain";
-import Gallery from "./Gallery";
+//import Gallery from "./Gallery";
 import FAQ from "./FAQ";
-import Testimonials from "./Testimonials";
+//import Testimonials from "./Testimonials";
 import Services from "./Services";
 import Login from "./login/Login";
 import Footer from "./Footer";
@@ -15,16 +15,16 @@ import Header1 from "./Header1";
 import HomePageHTML from "./HomePageHTML";
 
 import EditCustomerMain from "./editCustomer/EditCustomerMain";
-import UserAccount from './UserAccount';
 
 import ChangePasswordMain from './changePassword/ChangePasswordMain';
 
 import BookAppointmentMain from './bookAppointment/BookAppointmentMain';
-import { getQueriesForElement } from '@testing-library/react';
-import AddDogMain from './addDog/AddDogMain';
-import EditDogMain from './editDog/EditDogMain';
-import AddDog2 from './addDog/AddDog2';
-import AddDog1 from './addDog/AddDog1';
+//import { getQueriesForElement } from '@testing-library/react';
+//import AddDogMain from './addDog/AddDogMain';
+//import EditDogMain from './editDog/EditDogMain';
+//import AddDog2 from './addDog/AddDog2';
+//import AddDog1 from './addDog/AddDog1';
+import BookDaycareMain from './bookDaycare/BookDaycareMain';
 
 
 class App1 extends React.Component {
@@ -57,7 +57,7 @@ class App1 extends React.Component {
               <Route path="/Register" component={RegisterMain} />
               <Route path="/Services" component={Services} />
               <Route path="/Gallery" component={BookAppointmentMain} />
-              <Route path="/Testimonials" component={Testimonials} />
+              <Route path="/Testimonials" component={BookDaycareMain} />
               <Route path="/FAQ" component={FAQ} />
               <Route path="/ChangePass" component={ChangePasswordMain} />
               <Route path="/Profile" component={EditCustomerMain} />
