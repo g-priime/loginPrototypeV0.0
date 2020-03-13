c422bab2d5cdaf24b775800b88d8a3284ce7cc71
g-priime <57049481+g-priime@users.noreply.github.com>
2020-03-13 13:01:24 -0600

    Update BookAppointment1.js

--- src/components/bookAppointment/BookAppointment1.js
+++ src/components/bookAppointment/BookAppointment1.js
@@ -41,7 +41,7 @@ class BookAppointment1 extends React.Component {
           style={{ backgroundColor: "#ECEBE7 " }}
         >
           <div className="container bookAppointmentContainer">
-            <h1>Book Appointment</h1>
+            <h1>Book Boarding</h1>
 
             <div className="row">
               <div className="col-sm">
