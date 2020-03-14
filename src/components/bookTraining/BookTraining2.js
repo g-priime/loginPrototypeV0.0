8d972ff3a4af161f7174241bd0c8e37256abafcd
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-13 19:32:53 -0600

    Update BookTraining2.js

--- src/components/bookTraining/BookTraining2.js
+++ src/components/bookTraining/BookTraining2.js
@@ -1,26 +1,27 @@
 import React from "react";
 import "../../css/bookAppointment.css";
 import { Link } from "react-router-dom";
-import Moment from "moment";
+
+import Select from "react-select";
 
 class BookTraining2 extends React.Component {
   state = {
     dog: "",
     startTime: "",
-    endTime: "",
+    endTime: new Date(),
 
     dogs: [],
     comments: "",
-    selectedDogs: []
+    grooming: true,
+    selectedDogs: [],
+    selectedOption: null
   };
 
   onFormSubmit = event => {
     event.preventDefault();
 
-    console.log(this.state.dog);
-
     this.props.onSubmit(
-      this.state.selectedDogs,
+      this.state.dog,
       this.state.startTime,
       this.state.endTime,
       this.state.grooming,
@@ -40,114 +41,234 @@ class BookTraining2 extends React.Component {
           style={{ backgroundColor: "#ECEBE7 " }}
         >
           <div className="container bookAppointmentContainer">
-            <h1>Appointment Details</h1>
+            <h1>Book Training</h1>
 
-            <table className="table table-bordered">
-              <tbody>
-                <tr>
-                  <td>Dogs:</td>
-                  <td>
-                    {this.props.selectedDogs.map(doggy => (
-                      <div key={doggy.key.toString()}>{doggy.value}</div>
-                    ))}
-                  </td>
-                </tr>
+            <div className="row">
+              <div className="col-sm">
+                <label>Behaviours:</label>
+                <table className="table table-striped">
+                  <tbody>
+                    <tr>
+                      <td>Barking:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Chewing Destruction:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Counter Surfing:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Digging:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Jumping:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Pulling on Leash:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Building Confidence:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                    <tr>
+                      <td>Chewing:</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="Yes"
+                          checked={this.props.grooming === "Yes"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>Yes</td>
+                      <td>
+                        <input
+                          type="radio"
+                          name="grooming"
+                          value="No"
+                          checked={this.props.grooming === "No"}
+                          onChange={this.props.onChangeGrooming}
+                        />
+                      </td>
+                      <td>No</td>
+                    </tr>
+                  </tbody>
+                </table>
 
-                <tr>
-                  <td>Start Time:</td>
-                  <td>{Moment(this.props.startTime).format("MMMM Do YYYY, h:mm a")}</td>
-                </tr>
-
-                <tr>
-                  <td>End Time:</td>
-                  <td>{Moment(this.props.endTime).format("MMMM Do YYYY, h:mm a")}</td>
-                </tr>
-
-                <tr>
-                  <td>Grooming Added:</td>
-                  <td>{this.props.grooming}</td>
-                </tr>
-
-                <tr>
-                  <td>Additional Comments:</td>
-                  <td>{this.props.comments}</td>
-                </tr>
-
-                <tr>
-                  <td>Cost:</td>
-                  <td>${this.props.cost}</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <div className="bookAppointmentFooter">
-              <div className="row">
-                <div className="col-sm">
-                  <div className="d-flex justify-content-between">
-                    <Link
-                      to="/Services"
-                      type="button"
-                      className="btn mb-3"
-                      style={{
-                        fontWeight: "bold",
-                        backgroundColor: "#1D3461",
-                        color: "#ECEBE7",
-                        boxShadow:
-                          "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                      }}
-                    >
-                      Cancel Appointment
-                    </Link>
-                    <button
-                      onClick={this.props.proceedToPayment}
-                      className="btn mb-3"
-                      style={{
-                        fontWeight: "bold",
-                        backgroundColor: "#1D3461",
-                        color: "#ECEBE7",
-                        boxShadow:
-                          "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                      }}
-                    >
-                      Proceed To Payment
-                    </button>
-                  </div>
-                </div>
+                <br />
               </div>
+              <br />
+              <br />
+            </div>
+            <br />
 
-              <div className="row">
-                <div className="col-sm">
-                  <div className="d-flex justify-content-between">
-                    <button
-                      type="button"
-                      onClick={this.props.onClick}
-                      className="btn mb-3"
-                      style={{
-                        fontWeight: "bold",
-                        backgroundColor: "#1D3461",
-                        color: "#ECEBE7",
-                        boxShadow:
-                          "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                      }}
-                    >
-                      Modify Information
-                    </button>
-
-                    <button
-                      className="btn mb-3"
-                      style={{
-                        fontWeight: "bold",
-                        backgroundColor: "#1D3461",
-                        color: "#ECEBE7",
-                        boxShadow:
-                          "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                      }}
-                    >
-                      Book Now Pay Later
-                    </button>
-                  </div>
-                </div>
-              </div>
+            <div className="d-flex justify-content-between">
+              <Link
+                to="/Services"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+                }}
+              >
+                Cancel
+              </Link>
+              <button
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+                }}
+              >
+                Continue
+              </button>
             </div>
           </div>
         </form>
