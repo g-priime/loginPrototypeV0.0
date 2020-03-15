843541c9d8e21e1e23f81b21521587c0e49f66d1
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-14 21:49:25 -0600

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,8 +11,27 @@ import {
 import "../css/services.css";
 //import boarding from '../images/boarding.jpg';
 import { Link } from "react-router-dom";
+import Popup from "./PopUp";
 
 class Services extends React.Component {
+  state = { message: "", cn: "", bgColor: 'blue' };
+
+ UNSAFE_componentWillMount() {
+    if (typeof this.props.location.state == "undefined" || this.props.location.state === null) {
+        this.setState({ message: "" });
+      } else {
+        this.setState({ message: this.props.location.state.message });
+        this.setState({ cn: "popup3" });
+        this.togglePopup();
+      }
+ }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
   render() {
     return (
       <div className="ui segment d-flex justify-content-around p-0 m-0 bg card-group">
@@ -258,6 +277,16 @@ class Services extends React.Component {
             </Link>
           </CardBody>
         </Card>
+        <div>
+          {this.state.showPopup ? (
+            <Popup
+              cn={this.state.cn}
+              text={this.state.message}
+              closePopup={this.togglePopup.bind(this)}
+              bgColor={this.state.bgColor}
+            />
+          ) : null}
+        </div>
       </div>
     );
   }
