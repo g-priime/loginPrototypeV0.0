663f3174e41922746900e34beddb3e614e833e07
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-16 16:47:38 -0600

    Update PendingAppointment.js

--- src/components/viewAppointments/PendingAppointment.js
+++ src/components/viewAppointments/PendingAppointment.js
@@ -18,7 +18,8 @@ class PendingAppointment extends React.Component {
               color: "#ECEBE7",
               boxShadow:
                 "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
-              width: 150
+              width: 150,
+              marginTop: "-3%"
             }}
           >
             Edit
@@ -33,7 +34,8 @@ class PendingAppointment extends React.Component {
               color: "#ECEBE7",
               boxShadow:
                 "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
-              width: 150
+              width: 150,
+              marginTop: "-3%"
             }}
           >
             Cancel
