d811a162460715bebd55df9e6d1e266c62d7c71d
Holly <hharnack@hotmail.com>
2020-03-16 12:54:39 -0600

    Create AdminTool.js

---
+++ src/components/admintool/AdminTool.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Popup from "../PopUp";
+import '../../css/admin.css';
+
+class AdminTool extends React.Component {
+
+  
+  render() {
+    return (
+      <div className="ui segment">
+        <div className="admin-nav">
+          
+        </div>
+        <div className="admin-container">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminTool;
