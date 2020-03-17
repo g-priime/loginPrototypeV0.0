16d47bd967b7752c9096f7f435fdb72efcffdb75
Holly <hharnack@hotmail.com>
2020-03-17 14:36:53 -0600

    Create ViewCalendar.js

---
+++ src/components/admintool/ViewCalendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ViewCalendar extends React.Component {
+
+    render() {
+        return (
+            <div className="ui segment">
+                view calendar
+            </div>
+        );
+    }
+}
+
+export default ViewCalendar;
