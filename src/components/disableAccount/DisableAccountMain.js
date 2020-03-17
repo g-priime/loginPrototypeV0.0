384949024f9662858153d496f6b8e909de4050aa
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-17 16:46:24 -0600

    Update DisableAccountMain.js

--- src/components/disableAccount/DisableAccountMain.js
+++ src/components/disableAccount/DisableAccountMain.js
@@ -1,41 +1,79 @@
 import React from "react";
 import DisableAccount1 from "./DisableAccount1";
 import BasePath from "../../api/BasePath";
+import Popup from "../PopUp";
+import { Redirect } from "react-router-dom";
 
 class DisableAccountMain extends React.Component {
-  state = { password: "" };
+  state = { response: "", password: "", showPopup: false, cn: "" };
 
   onSubmit = async () => {
-    var token = localStorage.getItem("token");
+    //add back in when done testing
+    //var token = localStorage.getItem("token");
     var password = this.state.password;
 
-    const response = await BasePath.put("/webresources/changepassword", {
-      token,
+    const response = await BasePath.put("/webresources/disableAccount", {
+      //token,
       password
     });
 
     this.setState({ response: response.data });
 
-    if (this.state.images === "Password is incorrect") {
+    if (this.state.response === "Password is incorrect") {
       this.setState({ cn: "popup4" });
       this.togglePopup();
     }
   };
 
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
   handleChangePassword = event => {
     this.setState({ password: event.target.value });
   };
 
+  logOut = () => {
+    localStorage.clear();
+    //this.setState({ initialStates: false, username: "" });
+  };
+
   render() {
-    return (
-      <div>
-        <DisableAccount1
-          password={this.state.password}
-          onChangePassword={this.handleChangePassword}
-          onSubmit={this.onSubmit}
-        />
-      </div>
-    );
+    if (this.state.response === "Disabled") {
+      return (
+        <div style={{ marginTop: "10px" }}>
+          {this.logOut}
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { message: "Password has been changed" }
+            }}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <DisableAccount1
+            password={this.state.password}
+            onChangePassword={this.handleChangePassword}
+            onSubmit={this.onSubmit}
+          />
+          <div>
+            {this.state.showPopup ? (
+              <Popup
+                cn={this.state.cn}
+                text={this.state.response}
+                closePopup={this.togglePopup.bind(this)}
+                bgColor="red"
+              />
+            ) : null}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
