75a2991f20da5075a39ae2741d6dadef9e382dce
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-17 21:20:30 -0600

    Update DisableAccount1.js

--- src/components/disableAccount/DisableAccount1.js
+++ src/components/disableAccount/DisableAccount1.js
@@ -13,65 +13,38 @@ class DisableAccount1 extends React.Component {
 
   render() {
     return (
-      <div
-        className="ui segment contChangePassword"
-        style={{ backgroundColor: "#ECEBE7" }}
-      >
-        <form
-          onSubmit={this.onFormSubmit}
-          className="ui form"
-          style={{ backgroundColor: "#ECEBE7 " }}
-        >
-          <div className="container">
-            <h1>Disable Account</h1>
+      <div className="m-4">
+        <form onSubmit={this.onFormSubmit} className="m-4 pt-3">
+          <label>Please verify password to disable account:</label>
+          <input
+            type="password"
+            name="password"
+            pattern="^[a-zA-Z1-9_*-]{1,20}$"
+            title="Cannot be longer then 20 Characters. Letters, numbers, only special characters '_' '*' '-' allowed"
+            placeholder="Enter password"
+            value={this.props.password}
+            onChange={this.props.onChangePassword}
+            required
+          />
 
-            <div className="row">
-              <div className="col-sm">
-                <label>Please verify password to disable account:</label>
-                <input
-                  type="password"
-                  name="password"
-                  pattern="^[a-zA-Z1-9_*-]{1,20}$"
-                  title="Cannot be longer then 20 Characters. Letters, numbers, only special characters '_' '*' '-' allowed"
-                  placeholder="Enter password"
-                  value={this.props.password}
-                  onChange={this.props.onChangePassword}
-                  required
-                />
-              </div>
-              <br />
-              <br />
-            </div>
-            <br />
+          <br />
+          <br />
 
-            <div className="d-flex justify-content-between">
-              <Link
-                to="/Profile"
-                type="button"
-                className="btn mb-3"
-                style={{
-                  fontWeight: "bold",
-                  backgroundColor: "#1D3461",
-                  color: "#ECEBE7",
-                  boxShadow:
-                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                }}
-              >
-                Back to Profile
-              </Link>
-              <button
-                className="btn mb-3"
-                style={{
-                  fontWeight: "bold",
-                  backgroundColor: "#1D3461",
-                  color: "#ECEBE7",
-                  boxShadow:
-                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                }}
-              >
-                Submit
-              </button>
-            </div>
+          <br />
+
+          <div>
+            <button
+              className="btn mb-3"
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+              }}
+            >
+              Disable Account
+            </button>
           </div>
         </form>
       </div>
