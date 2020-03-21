1d12c145ceb61db833c5879d9affb34d401639d5
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-21 00:53:03 -0600

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import "../css/gallery.css";
 
 class Gallery extends React.Component {
 
     render() {
         return (
-            <div className="ui segment">
-                Gallery
+            <div className="ui segment cont" style={{ backgroundColor: "#6298C8" }}>
+                <h1 className="txt">Check out our safe space!</h1>
             </div>
         );
     }
