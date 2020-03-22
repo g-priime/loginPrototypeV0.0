1396c95b6e38aab44af1361dcfaaf9a59ac671ca
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-22 00:48:51 -0600

    Update FAQ.js

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -66,7 +66,29 @@ class FAQ extends React.Component {
                 <i>
                   <b>Login/Register</b>
                 </i>{" "}
-                button on the top menu bar. 
+                button on the top menu bar. Enter your credentials - user name
+                and password. Click{" "}
+                <i>
+                  <b>Login</b>
+                </i>{" "}
+                button. Proceed to the{" "}
+                <i>
+                  <b>Account</b>
+                </i>{" "}
+                button on the top menu bar and click{" "}
+                <i>
+                  <b>Profile</b>
+                </i>{" "}
+                to enter your account or{" "}
+                <i>
+                  <b>View Appointments</b>
+                </i>{" "}
+                button to review your appointments information. To log out of
+                the system click{" "}
+                <i>
+                  <b>Log Out</b>
+                </i>{" "}
+                button.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -78,9 +100,14 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                To change you password you have to be logged in into the system.
+                Under your Profile click{" "}
+                <i>
+                  <b>Change Password</b>
+                </i>{" "}
+                button. In the opened window enter your old password, new
+                password, confirmation of the new password. If new and confirmed
+                passwords match the password will be changed successfully.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -92,9 +119,15 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                You can book the new appointment from the{" "}
+                <i>
+                  <b>Services</b>
+                </i>{" "}
+                tab of the top menu or in your account under the{" "}
+                <i>
+                  <b>View Appointments</b>
+                </i>{" "}
+                tab.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -106,9 +139,18 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                To book appointments you have to have dog/dogs under your
+                account added. To add a new dog to your account log in into the
+                system. Go to your Profile and click{" "}
+                <i>
+                  <b>Add new dog</b>
+                </i>{" "}
+                button. In the openned window enter the information about your
+                dog. All the fieald are required. Add the picture of your dog -
+                required. Go to the step 2 clicking related button. Fill out the
+                Pet behaviour Profile and enter the dates for the vaccines. If
+                the listed vaccines have expired you are not allowed to bring
+                your dog in order to keep other dogs in safety!
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -120,9 +162,12 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                To check the status of your appointment, to make a payment or
+                cancel the appointment go to the{" "}
+                <i>
+                  <b>View Appointments</b>
+                </i>{" "}
+                tab of your account.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -134,9 +179,16 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                To change your personal information go to your{" "}
+                <i>
+                  <b>Profile</b>
+                </i>{" "}
+                under Account tab of the top menu bar and click{" "}
+                <i>
+                  <b>Edit customer information</b>
+                </i>{" "}
+                button. Make your changes in the openned window. The user name
+                can not be changed! Save the changes.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -148,23 +200,42 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                To edit the information about your dog go to your profile under{" "}
+                <i>
+                  <b>Account</b>
+                </i>{" "}
+                tab of the top menu bar. Find the name of the dog you wish to be
+                changed and click{" "}
+                <i>
+                  <b>Edit</b>
+                </i>{" "}
+                button next to the name. Make the changes in the new window. All
+                the fields are required. Save your changes. To delete the dog
+                find the dog name to be deleted and click{" "}
+                <i>
+                  <b>Delete</b>
+                </i>{" "}
+                button next to the name of the dog.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionItemHeading>
               <AccordionItemButton>
-                What vaccines are mandotory?
+                What vaccines are mandatory?
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                In order of keeping your and other dogs in safety we insist that
+                the followed vaccines should be taken.
+                <ul>
+                  <li>DA2PP</li>
+                  <li>Rabies</li>
+                  <li>Bordetella</li>
+                </ul>
+                The expiration date of the listed vaccines should not be erlier
+                than the date of the last day of boarding.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -176,9 +247,10 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                If the date of the mandatory vaccines has expired it is your
+                responsibility to take them in advance before your visit. Don't
+                forget to update the date of the expiration dates under your
+                Profile.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -190,9 +262,19 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                The followed ways of payment are acceptable:
+                <ul>
+                  <li>PayPal</li>
+                  <li>Cash</li>
+                  <li>Check</li>
+                  <li>Email transfer</li>
+                </ul>
+                To pay by PayPal go to{" "}
+                <i>
+                  <b>View Appointments</b>
+                </i>{" "}
+                tab under Account, book the new appointment and follow
+                instructions.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
@@ -204,9 +286,17 @@ class FAQ extends React.Component {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+                You can cancel your booked appointment not later than 3 days
+                before the date of it. To cancel appointment go to{" "}
+                <i>
+                  <b>View Appointments</b>
+                </i>{" "}
+                tab under your account, find the appointment to be cancelled and
+                click{" "}
+                <i>
+                  <b>Cancel</b>
+                </i>{" "}
+                button next to it. No refunds are available.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
