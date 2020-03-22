4ed70ffe26c31e4830d6e4c32213847fb0e73262
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-22 14:57:34 -0600

    Create CalendarMain.js

---
+++ src/components/calendar/CalendarMain.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda
+} from "@syncfusion/ej2-react-schedule";
+
+class CalendarMain extends React.Component {
+  render() {
+    return (
+      <ScheduleComponent>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
+    );
+  }
+}
+
+export default CalendarMain;
