45a43153b6d488c41cbb9af62427621750655d17
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-21 22:25:11 -0600

    Update FAQ.js

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,14 +1,195 @@
-import React from 'react';
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel
+} from "react-accessible-accordion";
+import "../css/faq.css";
+//import 'react-accessible-accordion/dist/fancy-example.css';
 
 class FAQ extends React.Component {
-
-    render() {
-        return (
-            <div className="ui segment">
-                FAQ
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className="gallery-container ui segment cont "
+        style={{ backgroundColor: "#ECEBE7" }}
+      >
+        <h1 className="txt">FAQ</h1>
+        <Accordion>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                What harsh truths do you prefer to ignore?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+                occaecat ut occaecat consequat est minim minim esse tempor
+                laborum consequat esse adipisicing eu reprehenderit enim.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                Is free will real or just an illusion?
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <p>
+                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+                nostrud velit in irure cillum tempor laboris sed adipisicing eu
+                esse duis nulla non.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    );
+  }
 }
 
 export default FAQ;
