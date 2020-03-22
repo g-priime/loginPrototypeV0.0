bb9d556bea4f83016ed140b248e6c6fdbc5b1782
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-22 15:54:04 -0600

    Update CalendarMain.js

--- src/components/calendar/CalendarMain.js
+++ src/components/calendar/CalendarMain.js
@@ -7,12 +7,29 @@ import {
   WorkWeek,
   Month,
   Agenda
+  //EventSettingsModel
 } from "@syncfusion/ej2-react-schedule";
+//import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
 
 class CalendarMain extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.data = [
+      {
+        Id: 2,
+        Subject: "Daycare",
+        StartTime: new Date(2020, 2, 23, 10, 0),
+        EndTime: new Date(2020, 2, 23, 12, 30)
+      }
+    ];
+  }
+
   render() {
     return (
-      <ScheduleComponent>
+      <ScheduleComponent
+        currentView="Month"
+        eventSettings={{ dataSource: this.data }}
+      >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
       </ScheduleComponent>
     );
