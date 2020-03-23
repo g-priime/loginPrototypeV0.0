22146ace6af4c62f0ad183fdd63f5e726415ad54
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-03-23 13:59:16 -0600

    Update CalendarMain.js

--- src/components/calendar/CalendarMain.js
+++ src/components/calendar/CalendarMain.js
@@ -12,7 +12,14 @@ import {
 //import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
 import BasePath from "../../api/BasePath";
 //import { WebinarData } from './dataSource';
-import { Internationalization, extend } from "@syncfusion/ej2-base";
+import {
+  Internationalization,
+  extend,
+  createElement
+} from "@syncfusion/ej2-base";
+import { DropDownList } from '@syncfusion/ej2-dropdowns'
+import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
 class CalendarMain extends React.Component {
   constructor() {
@@ -46,6 +53,7 @@ class CalendarMain extends React.Component {
         appointment.EndTime = result.data[i].endTime;
         appointment.dogIdNumber = result.data[i].dogIdNumber;
         appointment.username = result.data[i].username;
+        appointment.EventType = result.data[i].type;
         this.data.push(appointment);
       }
       //this.setState({ appointments: appointments });
@@ -53,57 +61,115 @@ class CalendarMain extends React.Component {
     }
   };
 
-  getTimeString(value){
-    return this.instance.formatDate(value, { skeleton: 'hm' })
+  getTimeString(value) {
+    return this.instance.formatDate(value, { skeleton: "hm" });
   }
 
   eventTemplate(props) {
-    if (props.Subject === "daycare"){
+    if (props.Subject === "daycare") {
       return (
-        <div
-          className="template-wrap"
-          style={{ background: "red" }}
-        >
+        <div className="template-wrap" style={{ background: "red" }}>
           <div className="subject" style={{ background: "red" }}>
             {props.Subject}
           </div>
           <div className="time" style={{ background: "red" }}>
-            Time: {this.getTimeString(props.StartTime)} - {this.getTimeString(props.EndTime)}
+            Time: {this.getTimeString(props.StartTime)} -{" "}
+            {this.getTimeString(props.EndTime)}
           </div>
         </div>
       );
-    }
-    else if (props.Subject === "boarding") {
+    } else if (props.Subject === "boarding") {
       return (
-        <div
-          className="template-wrap"
-          style={{ background: "green" }}
-        >
+        <div className="template-wrap" style={{ background: "green" }}>
           <div className="subject" style={{ background: "green" }}>
             {props.Subject}
           </div>
           <div className="time" style={{ background: "green" }}>
-            Time: {this.getTimeString(props.StartTime)} - {this.getTimeString(props.EndTime)}
+            Time: {this.getTimeString(props.StartTime)} -{" "}
+            {this.getTimeString(props.EndTime)}
           </div>
         </div>
       );
-    }
-    else {
+    } else {
       return (
-        <div
-          className="template-wrap"
-          style={{ background: "orange" }}
-        >
+        <div className="template-wrap" style={{ background: "orange" }}>
           <div className="subject" style={{ background: "orange" }}>
             {props.Subject}
           </div>
           <div className="time" style={{ background: "orange" }}>
-            Time: {this.getTimeString(props.StartTime)} - {this.getTimeString(props.EndTime)}
+            Time: {this.getTimeString(props.StartTime)} -{" "}
+            {this.getTimeString(props.EndTime)}
           </div>
         </div>
       );
     }
   }
+/*
+  onPopupOpen(args) {
+    if (args.type === "Editor") {
+      if (!args.element.querySelector(".custom-field-row")) {
+        let row = createElement("div", { className: "custom-field-row" });
+        let formElement = args.element.querySelector(".e-schedule-form");
+        formElement.lastChild.insertBefore(
+          row,
+          formElement.lastChild.lastChild
+        );
+        let container = createElement("div", {
+          className: "custom-field-container"
+        });
+        let inputEle = createElement("input", {
+          className: "e-field",
+          attrs: { name: "EventType" }
+        });
+        
+        container.appendChild(inputEle);
+        row.appendChild(container);
+        let drowDownList = new DropDownList({
+          dataSource: [
+            { text: "Public Event", value: "public-event" },
+            { text: "Maintenance", value: "maintenance" },
+            { text: "Commercial Event", value: "commercial-event" },
+            { text: "Family Event", value: "family-event" }
+          ],
+          fields: { text: "text", value: "value" },
+          value: args.data.EventType,
+          floatLabelType: "Always",
+          placeholder: "Event Type"
+        });
+        drowDownList.appendTo(inputEle);
+        inputEle.setAttribute("name", "EventType");
+        
+      }
+    }
+  }
+*/
+onPopupOpen(args) {
+  if (args.type === 'Editor') {
+      let statusElement = args.element.querySelector('#EventType');
+      statusElement.setAttribute('name', 'EventType');
+  }
+}
+editorTemplate(props) {
+  return (props !== undefined ? <table className="custom-event-editor" style={{ width: '100%', cellpadding: '5' }}><tbody>
+<tr><td className="e-textlabel">Appointment Type</td><td colSpan={4}>
+  <input id="Appointment Type" className="e-field e-input" type="text" name="Subject" style={{ width: '100%' }}/>
+</td></tr>
+<tr><td className="e-textlabel">Owner</td><td colSpan={4}>
+  <input id="Owner" className="e-field e-input" type="text" name="username" style={{ width: '100%' }}/>
+</td></tr>
+<tr><td className="e-textlabel">Status</td><td colSpan={4}>
+  <DropDownListComponent id="EventType" placeholder='Choose status' data-name="EventType" className="e-field" style={{ width: '100%' }} dataSource={['New', 'Requested', 'Confirmed']} value={props.EventType || null}></DropDownListComponent>
+</td></tr>
+<tr><td className="e-textlabel">From</td><td colSpan={4}>
+  <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent>
+</td></tr>
+<tr><td className="e-textlabel">To</td><td colSpan={4}>
+  <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
+</td></tr>
+<tr><td className="e-textlabel">Reason</td><td colSpan={4}>
+  <textarea id="Description" className="e-field e-input" name="Description" rows={3} cols={50} style={{ width: '100%', height: '60px !important', resize: 'vertical' }}></textarea>
+</td></tr></tbody></table> : <div></div>);
+}
 
   render() {
     this.getAppointmentInfo();
@@ -119,6 +185,8 @@ class CalendarMain extends React.Component {
             location: { name: "username", title: "Owner" }
           }
         }}
+        editorTemplate={this.editorTemplate.bind(this)}
+        popupOpen={this.onPopupOpen.bind(this)}
       >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
       </ScheduleComponent>
