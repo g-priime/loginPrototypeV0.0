f02d8b35fbe46c585549f6dbcd29e1333a303432
Holly <hharnack@hotmail.com>
2020-03-23 09:59:16 -0600

    Update AdminTool.js

--- src/components/admintool/AdminTool.js
+++ src/components/admintool/AdminTool.js
@@ -8,33 +8,35 @@ class AdminTool extends React.Component {
 
   render() {
     return (
-      <div className="ui segment">
+      <div style={{ backgroundColor: "rgba(255,255,255,0)" }} className="admin-container">
         <h1 style={{ color: "white" }}>K9 Fun Family Admin Tool</h1>
-        <div className="ui segment">
-          <div>
-            <Link
-              to="ViewCalendar"
-              type="button"
-              className="btn mb-3"
-              style={{
-                fontWeight: "bold",
-                backgroundColor: "#1D3461",
-                color: "#ECEBE7",
-                boxShadow:
-                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-              }}>View Calendar
+        <div style={{ backgroundColor: "rgba(255,255,255,0)" }}>
+          <div className="admin-row">
+            <div>
+              <Link
+                to="ViewCalendar"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+                }}>View Calendar
               </Link>
-          </div>
-          <div>
-            <BrowserRouter>
+            </div>
+            <div>
+              <BrowserRouter>
 
-              <div>
-                <div className="mt-4 ml-5 mr-5">
-                  <Route path="/ViewCalendar" exact component={ViewCalendar} />
+                <div>
+                  <div className="mt-4 ml-5 mr-5">
+                    <Route path="/ViewCalendar" exact component={ViewCalendar} />
+                  </div>
+
                 </div>
-
-              </div>
-            </BrowserRouter>
+              </BrowserRouter>
+            </div>
           </div>
         </div>
       </div>
