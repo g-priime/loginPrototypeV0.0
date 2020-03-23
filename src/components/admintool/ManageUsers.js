45945a0bd77dd4e040399e0dd90fe768ccf48cf1
Holly <hharnack@hotmail.com>
2020-03-23 11:40:50 -0600

    Create ManageUsers.js

---
+++ src/components/admintool/ManageUsers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ManageUsers extends React.Component {
+
+    render() {
+        return (
+            <div>
+                manage user
+            </div>
+        );
+    }
+}
+
+export default ManageUsers;
