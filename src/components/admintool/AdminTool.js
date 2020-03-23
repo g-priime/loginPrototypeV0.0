f24cf73848dee0a4533ba1d3d36dbcb0e1effe8d
Holly <hharnack@hotmail.com>
2020-03-23 07:47:58 -0600

    Update AdminTool.js

--- src/components/admintool/AdminTool.js
+++ src/components/admintool/AdminTool.js
@@ -5,13 +5,15 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import ViewCalendar from './ViewCalendar';
 
 class AdminTool extends React.Component {
-  
+
   render() {
     return (
-      <div className="ui segment admin">
-        <div className="admin-nav col-sm-4">
-          <Link
-              to="ViewCalendar"  
+      <div className="ui segment">
+        <h1 style={{ color: "white" }}>K9 Fun Family Admin Tool</h1>
+        <div className="ui segment">
+          <div>
+            <Link
+              to="ViewCalendar"
               type="button"
               className="btn mb-3"
               style={{
@@ -20,18 +22,20 @@ class AdminTool extends React.Component {
                 color: "#ECEBE7",
                 boxShadow:
                   "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-              }}>View Calendar</Link>
-        </div>
-        <div className="admin-container col-sm-6">
-        <BrowserRouter>
+              }}>View Calendar
+              </Link>
+          </div>
+          <div>
+            <BrowserRouter>
 
-<div>
-    <div className="mt-4 ml-5 mr-5">
-        <Route path="/ViewCalendar" exact component={ViewCalendar} />
-    </div>
+              <div>
+                <div className="mt-4 ml-5 mr-5">
+                  <Route path="/ViewCalendar" exact component={ViewCalendar} />
+                </div>
 
-</div>
-</BrowserRouter>
+              </div>
+            </BrowserRouter>
+          </div>
         </div>
       </div>
     );
