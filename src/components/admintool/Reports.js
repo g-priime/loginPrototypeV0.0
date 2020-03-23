4a1bc24346b7a0ff205e4b1930f19af320b14b0f
Holly <hharnack@hotmail.com>
2020-03-23 11:40:58 -0600

    Create Reports.js

---
+++ src/components/admintool/Reports.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Reports extends React.Component {
+
+    render() {
+        return (
+            <div>
+                reports
+            </div>
+        );
+    }
+}
+
+export default Reports;
