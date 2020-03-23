0d1cd86ba71fc839ed5f3229a17d5d648fb9af5a
Holly <hharnack@hotmail.com>
2020-03-23 11:40:27 -0600

    Create CreateUser.js

---
+++ src/components/admintool/CreateUser.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CreateUser extends React.Component {
+
+    render() {
+        return (
+            <div>
+                create user
+            </div>
+        );
+    }
+}
+
+export default CreateUser;
