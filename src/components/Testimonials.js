04d124e5342fca8b64b6c106e33a60ca621ff9b0
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-03-26 11:43:42 -0600

    Update Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React from "react";
 
 class Testimonials extends React.Component {
+  state = {
+    testimonialsList: [],
+    test_id: {},
+    username: {},
+    contents: ""
+  };
 
-    render() {
-        return (
-            <div className="ui segment">
-                Testimonials
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className="gallery-container ui segment cont "
+        style={{ backgroundColor: "#ECEBE7" }}
+      >
+        <h1 className="txt">Testimonials</h1>
+        <div>
+        {this.state.testimonialsList.map(testimonial => (
+            <Testimonials
+            key={testimonial.test_id}
+              testimonial={testimonial}
+              testimonialUsername={testimonial.username}
+              testimonialContent={testimonial.contents}
+            />
+            ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Testimonials;
