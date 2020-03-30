f6dc287c62c205af429aa4b40689e5d9e75c27ad
Holly <hharnack@hotmail.com>
2020-03-29 23:42:04 -0600

    Update Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,45 +4,82 @@ import '../css/testimonials.css';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 class Testimonials extends React.Component {
-  state = {
-    testimonialsList: [],
-    test_id: {},
-    username: {},
-    contents: ""
-  };
+    state = {
+        testimonialsList: [],
+        test_id: {},
+        username: {},
+        contents: ""
+    };
 
-  componentWillMount = () => {
-    BasePath.get('/webresources/Testimonials')
-    .then( result => {
-      this.setState({testimonialsList: result.data});
-      console.log(result);
-    }).catch(err => {
-      console.log(err);
-    });
-  }
-
-  render() {
-    return (
-      <div
-        className="gallery-container ui segment cont "
-        style={{ backgroundColor: "#ECEBE7" }}
-      >
-        <h1 className="txt">Testimonials</h1>
-        <ListGroup>
-          {this.state.testimonialsList.map(testimonial => (
-              <ListGroupItem>
-                <ListGroupItemHeading>{testimonial.username}</ListGroupItemHeading>
-                <ListGroupItemText>
-                <span className="wrap"> 
-   {testimonial.content}
-</span>
-               </ListGroupItemText>
-              </ListGroupItem>
-              ))}
-        </ListGroup>
-      </div>
-        );
-      }
+    componentWillMount = () => {
+        BasePath.get('/webresources/Testimonials')
+            .then(result => {
+                this.setState({ testimonialsList: result.data });
+                console.log(result);
+                console.log(localStorage.getItem('token'));
+            }).catch(err => {
+                console.log(err);
+            });
     }
-    
-    export default Testimonials;
+
+    submit = () => {
+        var token = localStorage.getItem('token');
+        BasePath.post('webresources/SubmitTestimonial',
+            {
+                token: token,
+                
+            }
+        );
+    }
+
+    contentChange = event => {
+        this.setState({contents: event.target.value});
+    }
+
+    render() {
+        return (
+            <div
+                className="gallery-container ui segment cont "
+                style={{ backgroundColor: "#ECEBE7" }}
+            >
+                <h1 className="txt">Testimonials</h1>
+                <ListGroup>
+                    {this.state.testimonialsList.map(testimonial => (
+                        <ListGroupItem>
+                            <ListGroupItemHeading>{testimonial.username}</ListGroupItemHeading>
+                            <ListGroupItemText>
+                                <span className="wrap">
+                                    {testimonial.content}
+                                </span>
+                            </ListGroupItemText>
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+                <h2>Add Testimonial</h2>
+                <form
+                onSubmit={this.submit}>
+                    <div className="form-group">
+                    <textarea className="form-control" value={this.content} onChange={this.changeContent}>
+                        Enter testimonial here...
+                    </textarea>
+                    </div>
+                    <br></br>
+                    <button
+                     className="btn mb-3"
+                     style={{
+                       fontWeight: "bold",
+                       backgroundColor: "#1D3461",
+                       color: "#ECEBE7",
+                       boxShadow:
+                         "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
+                     }}
+                      >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Testimonials;
