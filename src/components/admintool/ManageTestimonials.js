e325b91f495508aac52919b6670b54d5c91c239a
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-04-01 22:47:40 -0600

    Update ManageTestimonials.js

--- src/components/admintool/ManageTestimonials.js
+++ src/components/admintool/ManageTestimonials.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React from "react";
+import Testimonial from "./Testimonial";
+import BasePath from "../../api/BasePath";
 
 class ManageTestimonials extends React.Component {
+  state = {
+    testimonialsList: [],
+    test_id: {},
+    username: {},
+    contents: ""
+  };
 
-    render() {
-        return (
-            <div>
-                manage testimonials
-            </div>
-        );
-    }
+  componentWillMount = () => {
+    BasePath.get("/webresources/Testimonials")
+      .then(result => {
+        this.setState({ testimonialsList: result.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  approveTestimonial = testimonial => {
+
+  };
+
+  deleteTestimonial = testimonial => {};
+
+  render() {
+    return (
+      <div
+        className="ui segment p-3 mb-2 "
+        style={{ backgroundColor: "#ECEBE7" }}
+      >
+        <div>
+          <div>
+            <h3>Manage Testimonials</h3>
+            <br />
+          </div>
+          {this.state.testimonialsList.map(testimonial => (
+            <Testimonial
+              chosenTestimonial={testimonial.contents}
+              approveTestimonial={this.approveTestimonial}
+              deleteTestimonial={this.deleteTestimonial}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ManageTestimonials;
