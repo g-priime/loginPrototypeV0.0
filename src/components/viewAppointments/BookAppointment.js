da066fb0dfef6a19e17cafde4b7b815bab371c60
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-04-05 12:38:00 -0600

    Create BookAppointment.js

---
+++ src/components/viewAppointments/BookAppointment.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class BookAppointment extends React.Component {
+  render() {
+    return (
+      <div
+        className="ui segment contBookAppointmentOptions"
+        style={{ backgroundColor: "#ECEBE7" }}
+      >
+        <form
+          onSubmit={this.onFormSubmit}
+          className="ui form"
+          style={{ backgroundColor: "#ECEBE7 " }}
+        >
+          <div className="container bookAppointmentOptionsContainer">
+            <h1>Book Appointment</h1>
+
+            <div className="d-flex justify-content-between">
+              <Link
+                to="/ViewAppointments"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                }}
+              >
+                Cancel
+              </Link>
+              <Link
+                to="/BookBoarding"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                }}
+              >
+                Book Boarding
+              </Link>
+              <Link
+                to="/BookTraining"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                }}
+              >
+                Book Training
+              </Link>
+              <Link
+                to="/BookDaycare"
+                type="button"
+                className="btn mb-3"
+                style={{
+                  fontWeight: "bold",
+                  backgroundColor: "#1D3461",
+                  color: "#ECEBE7",
+                  boxShadow:
+                    "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                }}
+              >
+                Book Daycare
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default BookAppointment;
