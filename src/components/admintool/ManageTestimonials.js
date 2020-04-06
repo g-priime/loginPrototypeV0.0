db927dbfe2f11ce70946dbee3ac533fbcdd834e0
Holly <hharnack@hotmail.com>
2020-04-06 17:42:12 -0600

    Update ManageTestimonials.js

--- src/components/admintool/ManageTestimonials.js
+++ src/components/admintool/ManageTestimonials.js
@@ -6,18 +6,16 @@ import PopUpConfirm from "../PopUpConfirm";
 class ManageTestimonials extends React.Component {
   state = {
     testimonialsList: [],
-    test_id: {},
     testDel: "",
-    username: {},
     contents: "",
     showCon: false
   };
 
   componentWillMount = () => {
-    this.updateTestimoials();
+    this.updateTestimonials();
   };
 
-  updateTestimoials() {
+  updateTestimonials() {
     var token = localStorage.getItem("token");
     BasePath.get(`/webresources/PendingTestimonials/${token}`)
       .then(result => {
@@ -33,16 +31,17 @@ class ManageTestimonials extends React.Component {
     var id = testimonial.id;
     BasePath.put(`/webresources/ApproveTestimonial/${token}/${id}`).then(result => {
       console.log(result.data);
-      this.updateTestimoials();
+      this.updateTestimonials();
     });
   };
 
   deleteTest = () => {
     var token = localStorage.getItem('token');
+    console.log(token);
     var id = this.state.testDel.id;
     BasePath.put(`/webresources/DeleteTestimonial/${token}/${id}`).then(result => {
       console.log(result.data);
-      this.updateTestimoials();
+      this.updateTestimonials();
     });
   };
 
@@ -71,9 +70,6 @@ class ManageTestimonials extends React.Component {
             <h3>Manage Testimonials</h3>
             <br />
           </div>
-          {this.state.showCon && this.state.testDels != null ? (
-            <PopUpConfirm dontConfirm={this.dontConfirm} confirm={this.confirm} text={'Are you sure you want to delete ' + this.state.testDel.contents + '?'} cn="popup3" />
-          ) : null}
           {this.state.testimonialsList.map(testimonial => (
             <Testimonial
               chosenTestimonial={testimonial}
@@ -82,6 +78,9 @@ class ManageTestimonials extends React.Component {
             />
           ))}
         </div>
+        {this.state.showCon && this.state.testDel != null ? (
+            <PopUpConfirm dontConfirm={this.dontConfirm} confirm={this.confirm} text={'Are you sure you want to delete ' + this.state.testDel.content + '?'} cn="popup3" />
+          ) : null}
       </div>
     );
   }
