863003b9a913350530ec08919e20a2aa79d0ccc9
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-04-07 00:50:55 -0600

    Update AdminTool.js

--- src/components/admintool/AdminTool.js
+++ src/components/admintool/AdminTool.js
@@ -14,10 +14,10 @@ import { Redirect } from "react-router-dom";
 import ChangePassAdminMain from "./ChangePassAdminMain";
 
 class AdminTool extends React.Component {
-  state= {
-    loggedOut : false
-  }
-  
+  state = {
+    loggedOut: false,
+  };
+
   // logout = () => {
   //   localStorage.clear();
   //   this.props.authenticate(false);
@@ -27,64 +27,63 @@ class AdminTool extends React.Component {
   render() {
     if (this.state.loggedOut == true) {
       return (
-      <div style={{ marginTop: "10px" }}>
-      <Redirect
-        to= "/"
-      />
-    </div>
+        <div style={{ marginTop: "10px" }}>
+          <Redirect to="/" />
+        </div>
       );
     } else {
-    return (
-      <div
-        style={{ backgroundColor: "rgba(255,255,255,0)" }}
-        className="ui segment admin-container"
-      >
-        <BrowserRouter>
-          <h1 style={{ color: "white" }}>K9 Fun Family Admin Tool</h1>
-          <div style={{ backgroundColor: "rgba(255,255,255,0)" }}>
-            <div className="admin-row">
-              <div className="d-flex flex-column">
-                <Link
-                  to="ViewCalendar"
-                  type="button"
-                  className="btn mb-3"
-                  style={{
-                    fontWeight: "bold",
-                    backgroundColor: "#ECEBE7",
-                    boxShadow:
-                      "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                  }}
-                >
-                  View Calendar
-                </Link>
-                <Link
-                  to="CreateUser"
-                  type="button"
-                  className="btn mb-3"
-                  style={{
-                    fontWeight: "bold",
-                    backgroundColor: "#ECEBE7",
-                    boxShadow:
-                      "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                  }}
-                >
-                  Create user Account
-                </Link>
-                <Link
-                  to="ManageUsers"
-                  type="button"
-                  className="btn mb-3"
-                  style={{
-                    fontWeight: "bold",
-                    backgroundColor: "#ECEBE7",
+      return (
+        <div
+          style={{ backgroundColor: "rgba(255,255,255,0)" }}
+          className="ui segment admin-container"
+        >
+          <BrowserRouter>
+            <br />
+            <h1 style={{ color: "white" }}>K9 Fun Family Admin Tool</h1>
+            <div style={{ backgroundColor: "rgba(255,255,255,0)" }}>
+              <div className="admin-row">
+                <div className="d-flex flex-column">
+                  <Link
+                    to="ViewCalendar"
+                    type="button"
+                    className="btn mb-3"
+                    style={{
+                      fontWeight: "bold",
+                      backgroundColor: "#ECEBE7",
+                      boxShadow:
+                        "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    }}
+                  >
+                    View Calendar
+                  </Link>
+                  <Link
+                    to="CreateUser"
+                    type="button"
+                    className="btn mb-3"
+                    style={{
+                      fontWeight: "bold",
+                      backgroundColor: "#ECEBE7",
+                      boxShadow:
+                        "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    }}
+                  >
+                    Create user Account
+                  </Link>
+                  <Link
+                    to="ManageUsers"
+                    type="button"
+                    className="btn mb-3"
+                    style={{
+                      fontWeight: "bold",
+                      backgroundColor: "#ECEBE7",
 
-                    boxShadow:
-                      "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                  }}
-                >
-                  Manage Users
-                </Link>
-                {/* <Link
+                      boxShadow:
+                        "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    }}
+                  >
+                    Manage Users
+                  </Link>
+                  {/* <Link
                 to="ManagePhotos"
                 type="button"
                 className="btn mb-3"
@@ -96,21 +95,21 @@ class AdminTool extends React.Component {
                     "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
                 }}>Manage Photos
               </Link> */}
-                <Link
-                  to="ManageTestimonials"
-                  type="button"
-                  className="btn mb-3"
-                  style={{
-                    fontWeight: "bold",
-                    backgroundColor: "#ECEBE7",
+                  <Link
+                    to="ManageTestimonials"
+                    type="button"
+                    className="btn mb-3"
+                    style={{
+                      fontWeight: "bold",
+                      backgroundColor: "#ECEBE7",
 
-                    boxShadow:
-                      "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                  }}
-                >
-                  Manage Testimonials
-                </Link>
-                {/* <Link
+                      boxShadow:
+                        "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    }}
+                  >
+                    Manage Testimonials
+                  </Link>
+                  {/* <Link
                 to="Reports"
                 type="button"
                 className="btn mb-3"
@@ -121,21 +120,21 @@ class AdminTool extends React.Component {
                     "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
                 }}>Reports
               </Link> */}
-                <Link
-                  to="ChangePassAdminMain"
-                  type="button"
-                  className="btn mb-3"
-                  style={{
-                    fontWeight: "bold",
-                    backgroundColor: "#ECEBE7",
+                  <Link
+                    to="ChangePassAdminMain"
+                    type="button"
+                    className="btn mb-3"
+                    style={{
+                      fontWeight: "bold",
+                      backgroundColor: "#ECEBE7",
 
-                    boxShadow:
-                      "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                  }}
-                >
-                  Change Password
-                </Link>
-                {/* <button
+                      boxShadow:
+                        "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    }}
+                  >
+                    Change Password
+                  </Link>
+                  {/* <button
                   className="btn mb-3"
                   style={{
                     fontWeight: "bold",
@@ -147,51 +146,51 @@ class AdminTool extends React.Component {
                 >
                   Log Out
                 </button> */}
-              </div>
-              <div style={{ backgroundColor: "#ECEBE7" }} className="output">
-                {/* <BrowserRouter> */}
-                <div>
-                  <div className="mt-4 ml-5 mr-5">
-                    <Route
-                      path="/ViewCalendar"
-                      exact
-                      component={ViewCalendar}
-                    />
-                    <Route
-                      path="/CreateUser"
-                      exact
-                      component={CreateUserMain}
-                    />
-                    <Route
-                      path="/ManageUsers"
-                      exact
-                      component={ManageUsersEditMain}
-                    />
-                    <Route
-                      path="/ManagePhotos"
-                      exact
-                      component={ManagePhotos}
-                    />
-                    <Route
-                      path="/ManageTestimonials"
-                      exact
-                      component={ManageTestimonials}
-                    />
-                    <Route
-                      path="/ChangePassAdminMain"
-                      exact
-                      component={ChangePassAdminMain}
-                    />
-                    {/* <Route path="/Reports" exact component={Reports} /> */}
-                  </div>
                 </div>
-                {/* </BrowserRouter> */}
+                <div style={{ backgroundColor: "#ECEBE7" }} className="output">
+                  {/* <BrowserRouter> */}
+                  <div>
+                    <div className="mt-4 ml-5 mr-5">
+                      <Route
+                        path="/ViewCalendar"
+                        exact
+                        component={ViewCalendar}
+                      />
+                      <Route
+                        path="/CreateUser"
+                        exact
+                        component={CreateUserMain}
+                      />
+                      <Route
+                        path="/ManageUsers"
+                        exact
+                        component={ManageUsersEditMain}
+                      />
+                      <Route
+                        path="/ManagePhotos"
+                        exact
+                        component={ManagePhotos}
+                      />
+                      <Route
+                        path="/ManageTestimonials"
+                        exact
+                        component={ManageTestimonials}
+                      />
+                      <Route
+                        path="/ChangePassAdminMain"
+                        exact
+                        component={ChangePassAdminMain}
+                      />
+                      {/* <Route path="/Reports" exact component={Reports} /> */}
+                    </div>
+                  </div>
+                  {/* </BrowserRouter> */}
+                </div>
               </div>
             </div>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
+          </BrowserRouter>
+        </div>
+      );
     }
   }
 }
