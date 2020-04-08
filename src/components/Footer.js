860d5e207937d9634b2b19de03aac4a252a059a2
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-04-08 15:22:10 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,14 +10,14 @@ var style = {
   left: "0",
   bottom: "0",
   height: "80px",
-  width: "100%"
+  width: "100%",
 };
 
 var phantom = {
   display: "block",
   padding: "20px",
   height: "100px",
-  width: "100%"
+  width: "100%",
 };
 
 function Footer({ children }) {
