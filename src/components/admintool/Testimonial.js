52cdcbeee6806de66dbdfe837f8e412cc55c4b6d
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-04-08 15:20:39 -0600

    Update Testimonial.js

--- src/components/admintool/Testimonial.js
+++ src/components/admintool/Testimonial.js
@@ -1,45 +1,50 @@
-import React from 'react';
+import React from "react";
 import "../../css/testimonials.css";
 
 class Testimonial extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <div className="row borderTest" >
-                    <div className="col-sm-10 d-flex justify-content-between ">
-                    <span style={{fontSize:"large"}} className="mt-3 mr-3 wrapAdmin">{this.props.chosenTestimonial.content}</span>
-                <button  className="btn mr-3 mb-3 mt-3"
-                        style={{
-                          fontWeight: "bold",
-                          backgroundColor: "#1D3461",
-                          color: "#ECEBE7",
-                          height: "35px",
-                          boxShadow:
-                            "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                        }}
-                        onClick={this.props.approveTestimonial}>
-                Approve
-                </button>
-                </div>
-                <div className="col-sm">
-                <button  className="btn mr-3 mb-3 mt-3"
-                        style={{
-                          fontWeight: "bold",
-                          backgroundColor: "#1D3461",
-                          color: "#ECEBE7",
-                          height: "35px",
-                          boxShadow:
-                            "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
-                        }}
-                        onClick={this.props.deleteTestimonial}>
-                Delete
-                </button>
-                </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="row borderTest">
+          <div className="col-sm-10 d-flex justify-content-between ">
+            <span style={{ fontSize: "large" }} className="mt-3 mr-3 wrapAdmin">
+              {this.props.chosenTestimonial.content}
+            </span>
+            <button
+              className="btn mr-3 mb-3 mt-3"
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                height: "35px",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+              }}
+              onClick={this.props.approveTestimonial}
+            >
+              Approve
+            </button>
+          </div>
+          <div className="col-sm">
+            <button
+              className="btn mr-3 mb-3 mt-3"
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#1D3461",
+                color: "#ECEBE7",
+                height: "35px",
+                boxShadow:
+                  "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+              }}
+              onClick={this.props.deleteTestimonial}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Testimonial;
