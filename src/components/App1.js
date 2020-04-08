c4085a4394e86ca53c24c44f096277b2fbaad3dd
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-04-08 15:20:45 -0600

    Update App1.js

--- src/components/App1.js
+++ src/components/App1.js
@@ -1,40 +1,27 @@
 import React from "react";
-//import UserAccount from './UserAccount';
-import HomePage from './HomePage';
-import RegisterMain from './register/RegisterMain';
-import Gallery from './Gallery';
-import FAQ from './FAQ';
-import Testimonials from './Testimonials';
-import Services from './Services';
-import Login from './login/Login';
-import BasePath from './../api/BasePath';
-import '../css/main.css';
-import Admin from './admintool/AdminTool';
+import RegisterMain from "./register/RegisterMain";
+import Gallery from "./Gallery";
+import FAQ from "./FAQ";
+import Testimonials from "./Testimonials";
+import Services from "./Services";
+import Login from "./login/Login";
+import "../css/main.css";
 import Footer from "./Footer";
-import AdminTool from './admintool/AdminTool';
-//import "../css/main.css";
-
+import AdminTool from "./admintool/AdminTool";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header1 from "./Header1";
 import HomePageHTML from "./HomePageHTML";
-
-import ChangePasswordMain from './changePassword/ChangePasswordMain';
-
-import BookAppointmentMain from './bookAppointment/BookAppointmentMain';
-//import { getQueriesForElement } from '@testing-library/react';
-//import AddDogMain from './addDog/AddDogMain';
-//import EditDogMain from './editDog/EditDogMain';
-//import AddDog2 from './addDog/AddDog2';
-//import AddDog1 from './addDog/AddDog1';
-import BookDaycareMain from './bookDaycare/BookDaycareMain';
-import BookTrainingMain from './bookTraining/BookTrainingMain';
-import UserAccount from './UserAccount';
-import AddDog from './addDog/AddDogMain';
-import EditDog from './editDog/EditDogMain';
-import EditCustomer from './editCustomer/EditCustomerMain';
-import ViewAppointments from './viewAppointments/ViewAppointments';
+import ChangePasswordMain from "./changePassword/ChangePasswordMain";
+import BookAppointmentMain from "./bookAppointment/BookAppointmentMain";
+import BookDaycareMain from "./bookDaycare/BookDaycareMain";
+import BookTrainingMain from "./bookTraining/BookTrainingMain";
+import UserAccount from "./UserAccount";
+import AddDog from "./addDog/AddDogMain";
+import EditDog from "./editDog/EditDogMain";
+import EditCustomer from "./editCustomer/EditCustomerMain";
+import ViewAppointments from "./viewAppointments/ViewAppointments";
 import DisableAccountMain from "./disableAccount/DisableAccountMain";
-import EditAppointmentMain from './editAppointment/EditAppointmentMain';
+import EditAppointmentMain from "./editAppointment/EditAppointmentMain";
 import BookAppointment from "./viewAppointments/BookAppointment";
 
 class App1 extends React.Component {
@@ -42,7 +29,7 @@ class App1 extends React.Component {
     show: false,
     errMsg: "",
     showDisableAccount: false,
-    authenticated: false
+    authenticated: false,
   };
 
   toggleLogin = () => {
@@ -54,7 +41,7 @@ class App1 extends React.Component {
     this.setState({ errMsg: "" });
   };
 
-  changeErr = msg => {
+  changeErr = (msg) => {
     this.setState({ errMsg: msg });
   };
 
@@ -66,20 +53,25 @@ class App1 extends React.Component {
     this.setState({ showDisableAccount: false });
   };
 
-  authenticate= (value) => {
+  authenticate = (value) => {
     if (value) {
-      this.setState({authenticated: true});
+      this.setState({ authenticated: true });
     } else {
-      this.setState({authenticated: false});
+      this.setState({ authenticated: false });
     }
-  }
+  };
 
   render() {
     return (
       <div>
         <BrowserRouter>
           <div>
-            <Header1 showLogin={this.toggleLogin} authenticate={this.authenticate} authenticated={this.state.authenticated} showDisableAccount={this.toggleDisableAccount} />
+            <Header1
+              showLogin={this.toggleLogin}
+              authenticate={this.authenticate}
+              authenticated={this.state.authenticated}
+              showDisableAccount={this.toggleDisableAccount}
+            />
             <div className="mt-4 ml-5 mr-5">
               <Route path="/" exact component={HomePageHTML} />
               <Route path="/Register" component={RegisterMain} />
@@ -97,11 +89,13 @@ class App1 extends React.Component {
               <Route path="/EditDog" component={EditDog} />
               <Route path="/EditCustomer" component={EditCustomer} />
               <Route path="/DisableAccount" component={DisableAccountMain} />
-              <Route path="/admin" component={() => <AdminTool authenticate={this.authenticate}/>} />
+              <Route
+                path="/admin"
+                component={() => <AdminTool authenticate={this.authenticate} />}
+              />
               <Route path="/EditAppointment" component={EditAppointmentMain} />
               <Route path="/BookAppointment" component={BookAppointment} />
-              
-              
+
               <Login
                 show={this.state.show}
                 onHide={this.onHide}
@@ -116,7 +110,9 @@ class App1 extends React.Component {
             </div>
           </div>
         </BrowserRouter>
-        <footer><Footer /></footer>
+        <footer>
+          <Footer />
+        </footer>
       </div>
     );
   }
