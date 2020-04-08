88546936788877cfd105c8a0440e9d7748f50ca8
Viktoriia Davydova <769217@ACDM.DS.SAIT.CA>
2020-04-07 18:28:42 -0600

    Update Testimonial.js

--- src/components/admintool/Testimonial.js
+++ src/components/admintool/Testimonial.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../../css/testimonials.css";
 
 class Testimonial extends React.Component {
 
@@ -6,13 +7,14 @@ class Testimonial extends React.Component {
         return (
             <div>
                 <div className="row">
-                    <div className="col-sm d-flex justify-content-between">
-                    <span style={{fontSize:"large"}} className="mr-3">{this.props.chosenTestimonial.content}</span>
+                    <div className="col-sm-10 d-flex justify-content-between ">
+                    <span style={{fontSize:"large"}} className="mr-3 wrapAdmin">{this.props.chosenTestimonial.content}</span>
                 <button  className="btn mr-3 mb-3"
                         style={{
                           fontWeight: "bold",
                           backgroundColor: "#1D3461",
                           color: "#ECEBE7",
+                          height: "35px",
                           boxShadow:
                             "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
                         }}
@@ -26,6 +28,7 @@ class Testimonial extends React.Component {
                           fontWeight: "bold",
                           backgroundColor: "#1D3461",
                           color: "#ECEBE7",
+                          height: "35px",
                           boxShadow:
                             "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
                         }}
