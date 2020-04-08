b52204c3dc08533a5ef803f0491b4d934c99d6dc
Levon Rose <553185@ACDM.DS.SAIT.CA>
2020-04-08 12:02:24 -0600

    Update EditTraining3.js

--- src/components/editAppointment/EditTraining3.js
+++ src/components/editAppointment/EditTraining3.js
@@ -267,7 +267,7 @@ class EditTraining3 extends React.Component {
                         "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)"
                     }}
                   >
-                    Book Now Pay Later
+                    Update Appointment
                   </button>
                 </div>
               </div>
